035bbb7c4644cd9d05691581143c53dd553805f2
ws <wangshuai@wasu.com>
2026-02-22 22:50:46 +0800

    删除无效代码

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  author: 'Elwood Edwards',
   headerTitle: 'ElwoodEdwards',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
